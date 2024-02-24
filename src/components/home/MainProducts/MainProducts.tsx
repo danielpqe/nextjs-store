1238fe030e3148c10641357b9ade7d7567786dc2
Daniel Quispe <danielpqe@gmail.com>
2024-02-23 19:04:59 -0500

    fetching from server side

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,7 +1,36 @@
-export const MainProducts = () => {
+// import { useState } from "react";
+
+const getProducts = async () => {
+  const response = await fetch(
+    `${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json?`,
+    {
+      headers: {
+        "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
+      },
+    }
+  );
+  return await response.json();
+};
+
+type ProductsProps = {
+  id: string;
+  title: string;
+};
+
+export const MainProducts = async () => {
+  // const [products, setProducts] = useState<Array<ProductsProps>>([]);
+  const productsResponse = await getProducts();
+  console.log(productsResponse);
+  // setProducts(productsResponse.products);
+
   return (
     <footer>
       <h2>Main Products</h2>
+      {/* <ul>
+        {products.map((product) => {
+          return <li key={product.id}>{product.title}</li>;
+        })}
+      </ul> */}
     </footer>
   );
 };
